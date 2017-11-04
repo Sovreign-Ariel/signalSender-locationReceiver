a75738d28c638015a947e5ee7ef8f3a9be1dab93
Jose Crescncio <jcrescencio@csumb.edu>
2017-11-04 20:17:06 +0000

    Html Flask Skeleton

--- website.py
+++ website.py
@@ -1,14 +1,35 @@
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for # imports libraries for flask web development
+
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello World!"
 
 d = {"username": 'jCrescencio', "password": '23four' }
+u = {"username": '', "password":''}
 
-print(d["username"])
-print(hello())
+message = ""
 
-app = Flask(__name__)
+def checkValid(user,dictionary):
+    if (user.username == dictionary.username and user.password == dictionary.password):
+        return True
+    return False
+    
 
+#Homepage#######################################
+@app.route('/') # homepage route 
+def loginPage():
+    return render_template("loginPage.html") # renders homepage
+@app.route('/', methods=['POST'])
+def login_process(): # function that processes POST method
+    u.username = request.form['username'] 
+    u.password = request.form['password']
+    if(checkValid(u,d)):
+        return redirect(url_for(messageInput)) # passes email into following "feature" template
+    return redirect(url_for(loginPage))
+@app.route('/login_success')
+def messageInput():
+    return render_template("messageInput.html")
+@app.route('login_success')
+def success_process():
+    message = request.form['userM']
+    return redirect(url_for(messageInput))
+    
